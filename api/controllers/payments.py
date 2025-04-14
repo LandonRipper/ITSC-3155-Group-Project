5f2014d74e46f7873de13af71a779b51a9843317
LPhinney1 <lphinne1@uncc.edu>
2025-04-14 15:21:00 -0400

    added controller skeleton code

---
+++ api/controllers/payments.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from ..models import payments as model
+
+def create(db: Session):
+    return
+
+def read_all(db: Session):
+    return
+
+def read_one(db: Session):
+    return
+
+
+def update(db: Session):
+    return
+
+
+def delete(db: Session):
+    return
