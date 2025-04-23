523ed8a6d3120680d19a05128412ca56a9e46522
LPhinney1 <lphinne1@uncc.edu>
2025-04-23 15:11:11 -0400

    removed comments

--- api/controllers/menu_items.py
+++ api/controllers/menu_items.py
@@ -3,7 +3,6 @@ from ..models.menu_items import MenuItems
 from ..schemas.menu_items import MenuItemCreate, MenuItemUpdate
 from sqlalchemy import exc
 
-# Create a new menu item
 def create_menu_item(db: Session, menu_item: MenuItemCreate):
     db_menu_item = MenuItems(**menu_item.dict())
     db.add(db_menu_item)
@@ -11,15 +10,12 @@ def create_menu_item(db: Session, menu_item: MenuItemCreate):
     db.refresh(db_menu_item)
     return db_menu_item
 
-# Get a menu item by its ID
 def get_menu_item(db: Session, menu_item_id: int):
     return db.query(MenuItems).filter(MenuItems.id == menu_item_id).first()
 
-# Get all menu items
 def get_menu_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(MenuItems).offset(skip).limit(limit).all()
 
-# Update a menu item by ID
 def update_menu_item(db: Session, menu_item_id: int, menu_item: MenuItemUpdate):
     db_menu_item = db.query(MenuItems).filter(MenuItems.id == menu_item_id).first()
     if db_menu_item:
@@ -29,7 +25,6 @@ def update_menu_item(db: Session, menu_item_id: int, menu_item: MenuItemUpdate):
         db.refresh(db_menu_item)
     return db_menu_item
 
-# Delete a menu item by ID
 def delete_menu_item(db: Session, menu_item_id: int):
     db_menu_item = db.query(MenuItems).filter(MenuItems.id == menu_item_id).first()
     if db_menu_item:
