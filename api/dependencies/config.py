42c7d84e671f7acb087b3d613d2488f7b0c43700
LandonRipper <landonripper@hotmail.com>
2025-04-23 15:21:52 -0400

    fixing problems

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,5 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "Lripper7717"
     app_host = "localhost"
-    app_port = 8000
