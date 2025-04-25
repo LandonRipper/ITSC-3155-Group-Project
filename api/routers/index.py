09cee09e15bb292f3caa31c3c4fb5feef82a5b02
LPhinney1 <lphinne1@uncc.edu>
2025-04-25 15:46:24 -0400

    updated index.py routes

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,12 @@
-from . import orders, order_details, customers, payments
+from . import orders, order_details, customers, payments, recipes, resources, promotions, menu_items, feedback
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(customers.router)
     app.include_router(payments.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
+    app.include_router(promotions.router)
+    app.include_router(menu_items.router)
+    app.include_router(feedback.router)
