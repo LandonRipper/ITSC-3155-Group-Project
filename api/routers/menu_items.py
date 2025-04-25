e2cb5b4baab1d5d39f780674a0398b20d8b3e657
mdeguzm2 <deg.matthew@gmail.com>
2025-04-25 16:19:39 -0400

    fixed router naming
    fixed calling wrong controller function name in router for menu items

--- api/routers/menu_items.py
+++ api/routers/menu_items.py
@@ -10,22 +10,22 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=schema.MenuItemOut)
+@router.post("/", response_model=schema.MenuItem)
 def create(request: schema.MenuItemCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
 
-@router.get("/", response_model=list[schema.MenuItemOut])
+@router.get("/", response_model=list[schema.MenuItem])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
-@router.get("/{item_id}", response_model=schema.MenuItemOut)
+@router.get("/{item_id}", response_model=schema.MenuItem)
 def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db=db, item_id=item_id)
 
 
-@router.put("/{item_id}", response_model=schema.MenuItemOut)
+@router.put("/{item_id}", response_model=schema.MenuItem)
 def update(item_id: int, request: schema.MenuItemUpdate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, item_id=item_id)
 
