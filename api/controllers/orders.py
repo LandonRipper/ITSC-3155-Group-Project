431bf5820e00bda3365aa07959af742731470cfe
LandonRipper <landonripper@hotmail.com>
2025-04-25 16:42:50 -0400

    made the order table import the customer name

--- api/controllers/orders.py
+++ api/controllers/orders.py
@@ -1,13 +1,19 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import orders as model
+from ..models import customer as customer_model
 from sqlalchemy.exc import SQLAlchemyError
 
 
 def create(db: Session, request):
+    customer = db.query(customer_model.Customer).filter(customer_model.Customer.name == request.customer_name).first()
+    if not customer:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found!")
+
     new_item = model.Order(
-        customer_name=request.customer_name,
+        customer_id=customer.id,
         description=request.description
+
     )
 
     try:
@@ -15,11 +21,10 @@ def create(db: Session, request):
         db.commit()
         db.refresh(new_item)
     except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
+        error = str(e.__dict__["orig"])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
 
     return new_item
-
 
 def read_all(db: Session):
     try:
