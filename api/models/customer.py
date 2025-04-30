d5f3fb5dd9686f44c8c4b1d62a33a4f443de9429
LandonRipper <landonripper@hotmail.com>
2025-04-30 15:41:25 -0400

    Removed requirement for customer to input email making it nullable

--- api/models/customer.py
+++ api/models/customer.py
@@ -7,7 +7,7 @@ class Customer(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable = False)
-    email = Column(String(100),unique = True, nullable=False)
+    email = Column(String(100),unique = True)
     phone_number = Column(String(20))
     address = Column(String(200))
 
