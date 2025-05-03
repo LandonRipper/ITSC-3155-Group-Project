86d2742183606e05ba69d7d9c18954fa1dd3aaf4
LPhinney1 <lphinne1@uncc.edu>
2025-05-03 12:18:11 -0400

    added apply_promo route+controller and fixed payment DB

--- api/routers/payments.py
+++ api/routers/payments.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..controllers import payments as controller
@@ -10,10 +9,11 @@ router = APIRouter(
     tags=['Payments'],
     prefix="/payments"
 )
-@router.post("/payments/", response_model=PaymentCreate, status_code=201)
+
+
+@router.post("/", response_model=PaymentCreate, status_code=201)
 def create(request: schema.PaymentCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
-
 
 
 @router.get("/", response_model=list[schema.Payment])
@@ -36,9 +36,18 @@ def update(payment_id: int, request: schema.PaymentUpdate, db: Session = Depends
         raise HTTPException(status_code=404, detail="Payment not found!")
     return updated
 
+
 @router.delete("/{payment_id}", response_model=schema.Payment)
 def delete(payment_id: int, db: Session = Depends(get_db)):
     deleted = controller.delete(db=db, payment_id=payment_id)
     if deleted is None:
         raise HTTPException(status_code=404, detail="Payment not found!")
     return deleted
+
+
+@router.put("/{payment_id}/{promotion_id}", response_model=schema.Payment)
+def apply_promo(payment_id: int, promotion_id: int, db: Session = Depends(get_db)):
+    updated = controller.apply_promo(db=db, payment_id=payment_id, promotion_id=promotion_id)
+    if updated is None:
+        raise HTTPException(status_code=404, detail="Payment not found!")
+    return updated
